4f5571766ee950b53f41be6f30e0347bd8a43a41
Christian Chijioke <christianchijioke001@gmail.com>
2022-10-13 14:25:37 -0700

    proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,7 @@
 /* main is the beginning of a program*/
 /*return: returns 0 if no errors*/
 int main(void)/*main the beginning of a c program*/
+	/*the main function the beginning of a program*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
