0c00c2e47b349bee188de39b6a8eb0e7b7a2c3e5
Christian Chijioke <christianchijioke001@gmail.com>
2022-10-13 13:53:05 -0700

    using puts to print output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /*main is the beginning of a program in C*/
+/*puts function prints the output of a program*/
 int main(void)
 {
 	char MyStr[] = "Programming is like building a multilingual puzzle";
