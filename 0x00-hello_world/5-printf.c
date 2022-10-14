1f284c65dbbdee8d445a0e90018e5405ec5820a7
Christian Chijioke <christianchijioke001@gmail.com>
2022-10-13 20:00:42 -0700

    with proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * main is the entry point of each c program
  * return prints 0 if there are no errors
  */
 int main(void)
-/*main is the entry point of each c program*/
 {
 	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
