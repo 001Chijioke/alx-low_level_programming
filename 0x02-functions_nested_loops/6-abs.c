c19daa12b508408c340aa277758b4269c87fa289
Christian Chijioke <christianchijioke001@gmail.com>
2022-10-19 05:15:57 -0700

    No  such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ * @n: is the input integer
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	n = (n * n) / 2;
+	_putchar('n');
+}
