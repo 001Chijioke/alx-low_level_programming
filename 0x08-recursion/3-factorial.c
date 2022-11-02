c57352f87ec9b31f9d55bea602fefc0abf736d83
Christian Chijioke <christianchijioke001@gmail.com>
2022-11-02 13:17:28 -0700

    A program that computes the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: the number whose factorial is to be calculated
+ * Return: the factorial of a number
+ */
+int factorial(int n)
+{
+	int res = n;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n >= 0 && n <= 1)
+	{
+		return (1);
+	}
+}
