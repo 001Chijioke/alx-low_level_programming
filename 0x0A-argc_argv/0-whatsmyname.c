e43e4f9dea089d4c42b3b657ec61d0194d979cff
Christian Chijioke <christianchijioke001@gmail.com>
2022-11-04 08:32:38 -0700

    A function that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
-#include <stdout.h>
+#include <stdio.h>
 #include "main.h"
-
 /**
  * main - prints its own name
  * @argc: counts the argument
@@ -11,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%s\n", argv[]);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
