73b3d5179db89ba6310ac7c5e5a5132912aea1e1
Christian Chijioke <christianchijioke001@gmail.com>
2022-11-04 11:11:43 -0700

    A function that prints all the argument it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all the argument it receives
+ * @argc: counts the argument
+ * @argv: the arrgument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
