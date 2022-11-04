70963704cd9f36c6c5e98b254dcb77ae98c66a2f
Christian Chijioke <christianchijioke001@gmail.com>
2022-11-04 12:23:08 -0700

    A program that adds two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlin.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - checks if string is a digit
+ * @str: the string
+ * Return: Always 0
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - adds two positive numbers
+ * @argc: counts the argument
+ * @argv: the argument
+ * Return: Always
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
